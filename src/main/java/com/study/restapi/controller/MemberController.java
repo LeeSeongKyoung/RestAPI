b53746a3be8462fcd440ef3fe2d9defe3f98f86e
LeeSeongKyoung <llee_ss@naver.com>
2023-10-22 15:41:46 +0900

    화면 추가 및 기본 설정 추가

--- src/main/java/com/study/restapi/controller/MemberController.java
+++ src/main/java/com/study/restapi/controller/MemberController.java
@@ -1,13 +1,21 @@
 package com.study.restapi.controller;
 
+import com.study.restapi.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Map;
+
 @Slf4j
+@RestController
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
 
-
-
-
+    @GetMapping("/member")
+    public Map<String, Object> memberController() {
+        return memberService.getMemberData();
+    }
 }
--- src/main/java/com/study/restapi/service/MemberService.java
+++ src/main/java/com/study/restapi/service/MemberService.java
@@ -1,9 +1,9 @@
 package com.study.restapi.service;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Service
-@Slf4j
-public class MemberService {
+import java.util.Map;
+
+public interface MemberService {
+    Map<String, Object> getMemberData();
 }
