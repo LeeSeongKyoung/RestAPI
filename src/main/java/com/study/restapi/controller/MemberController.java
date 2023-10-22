1586c6d11632945323dfedea19f5d5e96d37aa76
LeeSeongKyoung <llee_ss@naver.com>
2023-10-22 15:11:34 +0900

    init

---
+++ src/main/java/com/study/restapi/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.study.restapi.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class MemberController {
+
+
+
+
+}
